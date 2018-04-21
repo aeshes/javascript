f25a244b08ed593ce23e502d54cb49b234427ec1
aeshes <mathesis@inbox.ru>
2018-04-21 18:54:30 +0300

    add test: exp 3 to 0

--- Mocha/Pow/test.js
+++ Mocha/Pow/test.js
@@ -3,10 +3,13 @@ var pow = require("./app/main");
 var assert = require("assert");
 
 describe("pow", function() {
-    it("Exponentiate 2 to the 3-rd", function() {
+    it("Exponentiate 2 to the 3-rd equals 8", function() {
         assert.equal(pow(2, 3), 8);
     });
-    it("Exponentiate 3 to the 4-th", function() {
+    it("Exponentiate 3 to the 4-th equals 81", function() {
         assert.equal(pow(3, 4), 81);
     });
+    it("Exponentiate 3 to the zero equals 1", function() {
+        assert.equal(pow(3, 0), 1);
+    })
 });
