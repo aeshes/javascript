47c6bed58807c848080a7d503bd444c31f1fac7c
aeshes <mathesis@inbox.ru>
2018-04-21 18:58:24 +0300

    some formatting

--- Mocha/Pow/test.js
+++ Mocha/Pow/test.js
@@ -3,13 +3,17 @@ var pow = require("./app/main");
 var assert = require("assert");
 
 describe("pow", function() {
+
     it("Exponentiate 2 to the 3-rd equals 8", function() {
         assert.equal(pow(2, 3), 8);
     });
+
     it("Exponentiate 3 to the 4-th equals 81", function() {
         assert.equal(pow(3, 4), 81);
     });
+
     it("Exponentiate 3 to the zero equals 1", function() {
         assert.equal(pow(3, 0), 1);
-    })
+    });
+    
 });
