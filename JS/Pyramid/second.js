141c20eadb6d5eddd944df7dc6c4813626d7eab4
aeshes <mathesis@inbox.ru>
2018-04-30 20:15:44 +0300

    pyramid on one side

--- JS/Pyramid/second.js
+++ JS/Pyramid/second.js
@@ -1,12 +1,19 @@
+"use strict";
+
 function repeat(str, times) {
     return times-- > 0 ? str + repeat(str, times) : "";
 }
 
 function drawPyramid(maxRows) {
-
+    for (let row = 1; row <= maxRows; ++row) {
+        let pattern = row < maxRows/2 ? repeat("*", row) : repeat("*", maxRows - row);
+        console.log(pattern);
+    }
 }
 
 console.log(repeat("#", 5));
 console.log(repeat("#", 0));
 console.log(repeat("#", 1));
 console.log(repeat("#", -1));
+
+drawPyramid(6);
