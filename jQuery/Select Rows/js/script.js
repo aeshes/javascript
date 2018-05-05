ca1ac1dff82f637b2004c5c2e1a11f4f38270d0b
aeshes <mathesis@inbox.ru>
2018-05-05 23:47:49 +0300

    Hide warning

--- jQuery/Select Rows/js/script.js
+++ jQuery/Select Rows/js/script.js
@@ -1,5 +1,9 @@
 (function() {
     $(document).ready(function() {
         $("#celebs tbody tr:even").addClass("zebra");
+        $("#disclaimer").addClass("warning");
+        $("#hide-button").click(function() {
+            $("#disclaimer").hide();
+        });
     });
 }());
